91e60be20c5b588b4f48de54aebd35870ee7d117
bak <alsebak@gmail.com>
2020-09-16 12:56:01 +0300

    feedback_form

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,11 +1,7 @@
 $( document ).ready(function() {
     $("#ajax_form").submit(function(e){
 		    e.preventDefault();
-		    /*$('#payemail').val($('#demo-email').val());
-		    $('#payfio').val($('#demo-name').val());
-		    $('#paytel').val($('#demo-phone').val());*/
 			sendAjaxForm('result_form', 'ajax_form', 'send-mail.php');
-			/*$('#payform').submit();*/
 			return false; 
 		}
 	);
@@ -25,13 +21,6 @@ function sendAjaxForm(result_form, ajax_form, url) {
             $('#result_form').html(response);//'Ошибка. Данные не отправлены.');
     	}
  	});
- 	//$(':input','#ajax_form','#demo-copy')
- //.not(':button, :submit, :reset, :hidden')
- //.val('')
-// .removeAttr("checked")
-// .removeAttr("selected");
  $('#ajax_form')[0].reset();
-
-
 setTimeout(function() {$('#result_form').html('');}, 5000);
 }
--- send-mail.php
+++ send-mail.php
@@ -1,13 +1,13 @@
 <?php
 if($_POST)
     {
-    $to = "alsebak@gmail.com, gs-822a@yandex.ru";//""; //куда отправлять письмо
+    $to = "info@bakinskii.com, alsebak@gmail.com";//""; //куда отправлять письмо
     $from = 'bakinskii.com'; // от кого
     $subject = "Обратная связь";
     
     $message = 'Имя: '.$_POST['name'].';<br>E-mail: '.$_POST['email'].';<br>Категория: '.$_POST['category'].';<br>Сообщение: '.$_POST['message'].';';
     $headers = "Content-type: text/html; charset=UTF-8 \r\n";
-    $headers .= "From: <info@sundayrussiaschool.com>\r\n";
+    $headers .= "From: <info@bakinskii.com>\r\n";
     $result = mail($to, $subject, $message, $headers);
     if ($result){
         echo "Спасибо за обращение. <br> В ближайшее время мы с вами свяжемся.";
